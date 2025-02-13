88500de06356deab319eb723480961ff2fad259a
Nusiek <norbert.furmanik@op.pl>
2025-02-13 02:32:23 +0100

    api key updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,43 @@ import Footer from './components/Footer';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [data, setData] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [showModal, setShowModal] = useState(false);
-  
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [showModal, setShowModal] = useState(false);
+
 	const handleToggleModal = () => {
-    setShowModal(!showModal);
+		setShowModal(!showModal);
 	};
-  
+
 	useEffect(() => {
-    const fetchAPIData = async () =>{
-      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
-      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+		const fetchAPIData = async () => {
+			const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+			const url =
+				'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`;
 
-      try{
-        const res = await fetch(url)
-        const apiData = await res.json()
-        setData(apiData)
-      } catch(err){
-        console.log(err.message);
-      }
-
-    }
-    fetchAPIData()
-  }, []);
+			try {
+				const res = await fetch(url);
+				const apiData = await res.json();
+				setData(apiData);
+			} catch (err) {
+				console.log(err.message);
+			}
+		};
+		fetchAPIData();
+	}, []);
 	return (
 		<>
-			{data ? (<Main data={data}/>) : (
-        <div className='loadingState'>
-          <i className="fa-solid fa-gear"></i>
-          </div>
-      )}
-			{showModal && <SideBar data={data} handleToggleModal={handleToggleModal} />}
-			{data && (<Footer data={data} handleToggleModal={handleToggleModal} />)}
+			{data ? (
+				<Main data={data} />
+			) : (
+				<div className='loadingState'>
+					<i className='fa-solid fa-gear'></i>
+				</div>
+			)}
+			{showModal && (
+				<SideBar data={data} handleToggleModal={handleToggleModal} />
+			)}
+			{data && <Footer data={data} handleToggleModal={handleToggleModal} />}
 		</>
 	);
 };
